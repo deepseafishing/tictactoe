7020c7d950e67d333e6834acd1c933fd2fdeb200
One June Kang <thisisforgraduateonly@gmail.com>
2019-11-19 12:14:27 -0500

    style: concept of sword vs shield

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { MaterialCommunityIcons as Icon } from 'react-native-vector-icons';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>The War of Sword and Shield</Text>
+
+      <View style={{ flexDirection: 'row' }}>
+        <TouchableOpacity
+          style={[styles.cell, { borderLeftWidth: 0, borderTopWidth: 0 }]}
+        >
+          <Icon name="shield" style={styles.shield} />
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.cell, { borderTopWidth: 0 }]}>
+          <Icon name="sword" style={styles.sword} />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.cell, { borderRightWidth: 0, borderTopWidth: 0 }]}
+        />
+      </View>
+      <View style={{ flexDirection: 'row' }}>
+        <TouchableOpacity style={[styles.cell, { borderLeftWidth: 0 }]} />
+        <TouchableOpacity style={styles.cell} />
+        <TouchableOpacity style={[styles.cell, { borderRightWidth: 0 }]} />
+      </View>
+      <View style={{ flexDirection: 'row' }}>
+        <TouchableOpacity
+          style={[styles.cell, { borderLeftWidth: 0, borderBottomWidth: 0 }]}
+        />
+        <TouchableOpacity style={[styles.cell, { borderBottomWidth: 0 }]} />
+        <TouchableOpacity
+          style={[styles.cell, { borderRightWidth: 0, borderBottomWidth: 0 }]}
+        />
+      </View>
     </View>
   );
 }
@@ -12,8 +41,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#292D3E',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  cell: {
+    color: 'black',
+    borderColor: '#82AAFF',
+    borderWidth: 3,
+    width: 130,
+    height: 130
+  },
+  shield: {
+    color: '#C3E88D',
+    fontSize: 100,
+    alignItems: 'center'
+  },
+  sword: {
+    color: '#FF5370',
+    fontSize: 100,
+    alignItems: 'center'
+  },
+  title: {
+    color: '#959DCB',
+    fontSize: 30,
+    paddingBottom: 100
   }
 });
